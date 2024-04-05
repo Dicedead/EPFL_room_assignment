41bb94a0c84c8dd4734a139e1c22679d94b873a6
Dicedead <dicedead@gmail.com>
2024-04-05 10:58:43 +0200

    initial classes

---
+++ architecture.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from enum import Enum
+
+WeekDay = Enum("WeekDay", ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"])
+Time = (int, int)
+
+
+@dataclass
+class Timeslot:
+    day: WeekDay
+    time: Time
+
+@dataclass
+class Room:
+    capacity: int
+    name: str
+
+
+@dataclass
+class Lecture:
+    num_participants: int
+    allocated_room: Room
+    timeslot: Timeslot
+    already_swapped: bool
