2618c5b923b7908688d44e710b76a7b0a4a46c40
Lucie Moulin <moulin.l@outlook.com>
2024-04-05 12:05:02 +0200

    corrected main

--- EPFL_room_assignment.py
+++ EPFL_room_assignment.py
@@ -1,4 +1,5 @@
 from architecture import Room, Timeslot, WeekDay, Lecture, Time
+from preprocessing import construct_all_lectures
 
 def print_swap(l1,l2):
     print(f"swap de {l1.code} ({l1.num_participants} personnes en {l1.allocated_room.name} (capacité : {l1.allocated_room.capacity}) le {l1.timeslot.day} de {l1.timeslot.time[0]} à {l1.timeslot.time[1]}) avec {l2.code} ({l2.num_participants} personnes en {l2.allocated_room.name} (capacité : {l2.allocated_room.capacity}))")
@@ -7,7 +8,7 @@ def find_better_room(lecture, all_lectures):
     L = [l for l in all_lectures if (lecture.timeslot == l.timeslot and not l.overbooked and not l.already_swapped and l.allocated_room.capacity >= lecture.num_participants) ]
     if len(L) > 0 :
         L.sort(key=lambda x: x.num_participants, reverse=True)
-        print_swap(l, L[0])
+        print_swap(lecture, L[0])
         temp = lecture.allocated_room
         lecture.allocated_room = L[0].allocated_room
         L[0].allocated_room = temp
@@ -16,15 +17,7 @@ def find_better_room(lecture, all_lectures):
         return L[0];
     return 0
 
-t = Timeslot(WeekDay.Monday, (10,11))
-poin = Room(50,"poin")
-ce = Room(500,"CE")
-co = Room(300,"CO")
-lectures = [
-    Lecture("Physique","nerdy things", 200,poin,t,False,True),
-    Lecture("Analyse","more nerdy things", 45,ce,t,False,False),
-    Lecture("ICC","extreme nerdiness", 350,co,t,False,True)
-    ]
+lectures = construct_all_lectures("data/course_bookings.csv", "data/courses_with_participants.csv")
 overbooked_lectures = [l for l in lectures if l.overbooked]
 overbooked_lectures.sort(key=lambda x: x.num_participants, reverse=True)
 for l in overbooked_lectures:
