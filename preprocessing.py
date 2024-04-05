c974f2b43c74f2e5b1159c07bcf73620aaed3b15
Dicedead <dicedead@gmail.com>
2024-04-05 11:59:58 +0200

    corrected preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -7,6 +7,14 @@ from typing import Optional
 from architecture import *
 
 
+def unique(list1):
+    unique_list = []
+    for x in list1:
+        if x not in unique_list:
+            unique_list.append(x)
+    return unique_list
+
+
 def entry_to_lecture(entry, course_participants) -> Optional[Lecture]:
     same_course_code = course_participants[course_participants["course_code"] == entry["course_code"]]
     if len(same_course_code) > 0:
@@ -14,7 +22,7 @@ def entry_to_lecture(entry, course_participants) -> Optional[Lecture]:
         if not np.isnan(num_participants):
             lecture = Lecture(
                 code=entry["course_code"],
-                title=entry["title"],
+                title=entry["course_name"],
                 num_participants=num_participants,
                 allocated_room=Room(entry["room_capacity"], entry["room_name"]),
                 timeslot=str_to_timeslot(entry["start_datetime"], entry["end_datetime"]),
@@ -28,7 +36,8 @@ def entry_to_lecture(entry, course_participants) -> Optional[Lecture]:
 def str_to_timeslot(start_str: str, end_str: str, datetime_format='%Y-%m-%d %H:%M:%S') -> Timeslot:
     start_datetime_obj = datetime.datetime.strptime(start_str, datetime_format)
     end_datetime_obj = datetime.datetime.strptime(end_str, datetime_format)
-    weekday = WeekDay[start_datetime_obj.weekday() - 1]
+    tmp = start_datetime_obj.weekday()
+    weekday = WeekDay(start_datetime_obj.weekday() + 1).name
     start_hour = start_datetime_obj.hour
     end_hour = end_datetime_obj.hour
 
@@ -49,5 +58,9 @@ def construct_all_lectures(course_bookings_path: str, course_participants_path: 
         if maybe_lect:
             all_lectures.append(maybe_lect)
 
-    all_lectures = list(set(all_lectures))
-    return all_lectures
+    return unique(all_lectures)
+
+
+if __name__ == "__main__":
+    all_lects = construct_all_lectures("data/course_bookings.csv", "data/courses_with_participants.csv")
+    print(len(all_lects))
