fa17aaf584fe0ed7be92d05b5f62e6689eadefca
Dicedead <dicedead@gmail.com>
2024-04-05 12:36:56 +0200

    redirected results to txt file

--- EPFL_room_assignment.py
+++ EPFL_room_assignment.py
@@ -1,3 +1,6 @@
+import sys
+sys.stdout = open('results/res.txt', 'w')
+
 from architecture import Lecture
 from preprocessing import construct_all_lectures
 
@@ -9,7 +12,7 @@ def print_swap(l1: Lecture, l2: Lecture):
           f"{l2.allocated_room.name} (capacity : {l2.allocated_room.capacity}))")
 
 
-def find_better_room(lecture, all_lectures):
+def find_better_room(lecture, all_lectures) -> bool:
     candidate_lectures = [swap_lecture for swap_lecture in all_lectures if
                           (lecture.timeslot == swap_lecture.timeslot
                            and not swap_lecture.overbooked
@@ -26,12 +29,22 @@ def find_better_room(lecture, all_lectures):
         candidate_lectures[0].allocated_room = temp
         lecture.overbooked = False
         lecture.already_swapped = True
-        return candidate_lectures[0]
-    return 0
+        return True
+    return False
 
 
 lectures = construct_all_lectures("data/course_bookings.csv", "data/courses_with_participants.csv")
 overbooked_lectures = [overbooked_lecture for overbooked_lecture in lectures if overbooked_lecture.overbooked]
 overbooked_lectures.sort(key=lambda x: x.num_participants, reverse=True)
-for candidate_lecture in overbooked_lectures:
-    find_better_room(candidate_lecture, lectures)
+print(f"Number of overbooked lectures: {len(overbooked_lectures)}")
+print("=========================================================================")
+print("=========================================================================")
+overbooked_lectures_no_solution = []
+for overbooked_lecture in overbooked_lectures:
+    if not find_better_room(overbooked_lecture, lectures):
+        overbooked_lectures_no_solution.append(overbooked_lecture)
+print("=========================================================================")
+print("=========================================================================")
+print(f"Number of overbooked lectures for which no solution is found: {len(overbooked_lectures_no_solution)}")
+print(f"Number of overbooked lectures for which a solution is found: "
+      f"{len(overbooked_lectures) - len(overbooked_lectures_no_solution)}")
